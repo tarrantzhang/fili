bf82f3dc420a1d600b7708799794095e33cff9a4
Jiaqi Liu <2257440489@qq.com>
2017-06-20 10:46:24 -0700

    Correct cache config comment & Test improvment

--- fili-core/src/test/groovy/com/yahoo/bard/webservice/web/handlers/workflow/DruidWorkflowSpec.groovy
+++ fili-core/src/test/groovy/com/yahoo/bard/webservice/web/handlers/workflow/DruidWorkflowSpec.groovy
@@ -37,6 +37,7 @@ class DruidWorkflowSpec extends Specification {
     private static final ObjectMapper MAPPER = new ObjectMapper()
             .registerModule(new Jdk8Module().configureAbsentsAsNulls(false))
     private static final SystemConfig SYSTEM_CONFIG = SystemConfigProvider.getInstance();
+    private static final String UNCOVERED_KEY = SYSTEM_CONFIG.getPackageVariableName("druid_uncovered_interval_limit")
 
     boolean cacheStatus
     boolean cacheV2Status
@@ -52,16 +53,10 @@ class DruidWorkflowSpec extends Specification {
     QuerySigningService<Long> querySigningService = Mock(SegmentIntervalsHashIdGenerator)
     VolatileIntervalsService volatileIntervalsService = Mock(VolatileIntervalsService)
 
-    SystemConfig systemConfig
-    String uncoveredKey
-
     def setup() {
         cacheStatus = DRUID_CACHE.isOn()
         cacheV2Status = DRUID_CACHE_V2.isOn()
         splittingStatus = QUERY_SPLIT.isOn()
-
-        systemConfig = SystemConfigProvider.getInstance()
-        uncoveredKey = SYSTEM_CONFIG.getPackageVariableName("druid_uncovered_interval_limit")
     }
 
     def cleanup() {
@@ -206,7 +201,7 @@ class DruidWorkflowSpec extends Specification {
 
     def "Test workflow contains DruidPartialDataRequestHandler when druidUncoveredIntervalLimit > 0"() {
         setup:
-        systemConfig.setProperty(uncoveredKey, '10')
+        SYSTEM_CONFIG.setProperty(UNCOVERED_KEY, '10')
         dw = new DruidWorkflow(
                 dataCache,
                 uiWebService,
@@ -232,14 +227,12 @@ class DruidWorkflowSpec extends Specification {
         handlers2.find(byClass(DruidPartialDataRequestHandler)) != null
 
         cleanup:
-        systemConfig.clearProperty(uncoveredKey)
+        SYSTEM_CONFIG.clearProperty(UNCOVERED_KEY)
     }
 
     def "Test workflow doesn't contain DruidPartialDataRequestHandler when druidUncoveredIntervalLimit <= 0"() {
         setup:
-        SystemConfig systemConfig = SystemConfigProvider.getInstance()
-        String uncoveredKey = SYSTEM_CONFIG.getPackageVariableName("druid_uncovered_interval_limit")
-        systemConfig.setProperty(uncoveredKey, '0')
+        SYSTEM_CONFIG.setProperty(UNCOVERED_KEY, '0')
         dw = new DruidWorkflow(
                 dataCache,
                 uiWebService,
@@ -265,6 +258,6 @@ class DruidWorkflowSpec extends Specification {
         handlers2.find(byClass(DruidPartialDataRequestHandler)) == null
 
         cleanup:
-        systemConfig.clearProperty(uncoveredKey)
+        SYSTEM_CONFIG.clearProperty(UNCOVERED_KEY)
     }
 }
